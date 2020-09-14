18036442f67d196bc0b621d8d655ac9f58adb8ec
Anthony Tineo <atineo@intellisys.com.do>
2020-09-13 21:07:29 -0400

    Add JavaScript part

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-class Photo {
-    constructor(imgUploader){
-        this.imgUploader
+document.getElementById('contenido').addEventListener('submit', savePost);
+
+function savePost(e) {
+    let textBlog = document.getElementById('textBlog').value;
+    let newPhoto = document.getElementById('newPhoto').value;
+    console.log(newPhoto);
+
+    const post = {
+        textBlog,
+        newPhoto
+    };
+
+    if (localStorage.getItem('posts') === null){
+        let posts = [];
+        posts.push(post);
+        localStorage.setItem('posts', JSON.stringify(posts));
+    }else {
+        let posts = JSON.parse(localStorage.getItem('posts'));
+        posts.push(post);
+        localStorage.setItem('posts', JSON.stringify(posts));
+    }
+
+    getPost();
+    document.getElementById('contenido').reset();
+    e.preventDefault();
+}
+
+function getPost() {
+    let posts = JSON.parse(localStorage.getItem('posts'));
+    let postsView = document.getElementById('element');
+
+    postsView.innerHTML = '';
+
+    for(let i = 0; i < posts.length; i++) {
+        let textBlog = posts[i].textBlog;
+        let newPhoto = posts[i].newPhoto;
+
+        postsView.innerHTML += `<img src="${newPhoto}" alt="examplePhoto" width= 800px height= 400px>
+        <p id="photoParagraph">${textBlog}</p>
+        `
     }
 }
 
-class PhotoBlog {
-    addPhoto(photo){
-        /*Cuadro donde estara la foto*/
-        const imgPreview = document.getElementsByClassName('element');
-        const element = document.createElement('div');
-        element.innerHTML = `
-        <img src="./${photo.imgUploader}" height="350px" width="350px">`;
-
-    imgPreview.appendChild(element);
-    }
-
-    resetBoton(){
-
-    }
-
-    deletePhoto(){
-
-    }
-
-    showMessage(){
-
-    }
-}
-
-/* Eventos del DOM */
-document.getElementById('botons').addEventListener("click", function(event) {
-    const newPhoto = document.getElementById('imgUploader').value;
-
-    const photo = new Photo(newPhoto);
-    /* Un nuevo objeto del PhotoBlog */
-    const photoBlog = new PhotoBlog();
-
-    photoBlog.addProduct(photo);
-    event.preventDefault();
-});
+getPost();
