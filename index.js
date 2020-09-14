d4861ae0c328c22afb4fae2b6b356595ccc6ef37
Anthony Tineo <atineo@intellisys.com.do>
2020-09-14 11:01:01 -0400

    Changes in the photoPost

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 document.getElementById('contenido').addEventListener('submit', savePost);
 
+/*Función para la imagen*/
+/*
+function preview(e)
+{
+	if(e.files && e.files[0])
+	{
+		// Comprobamos que sea un formato de imagen
+		if (e.files[0].type.match('image.*')) {
+ 
+			// Inicializamos un FileReader. permite que las aplicaciones web lean 
+			// ficheros (o información en buffer) almacenados en el cliente de forma
+			// asíncrona
+			var reader=new FileReader();
+ 
+			// El evento onload se ejecuta cada vez que se ha leido el archivo
+			// correctamente
+			reader.onload=function(e) {
+				document.getElementById("placePostImage").innerHTML="<img src='"+e.target.result+"' width= 800px height= 400px>";
+			}
+		}else{
+ 
+			// El formato del archivo no es una imagen
+			document.getElementById("placePostImage").innerHTML="No es un formato de imagen";
+		}
+	}
+}
+*/
+
 function savePost(e) {
     let textBlog = document.getElementById('textBlog').value;
     let newPhoto = document.getElementById('newPhoto').value;
-    console.log(newPhoto);
-
+    
     const post = {
         textBlog,
         newPhoto
@@ -27,17 +54,35 @@ function savePost(e) {
 
 function getPost() {
     let posts = JSON.parse(localStorage.getItem('posts'));
-    let postsView = document.getElementById('element');
+    let postsView = document.getElementById('placePost');
 
     postsView.innerHTML = '';
 
     for(let i = 0; i < posts.length; i++) {
         let textBlog = posts[i].textBlog;
         let newPhoto = posts[i].newPhoto;
-
-        postsView.innerHTML += `<img src="${newPhoto}" alt="examplePhoto" width= 800px height= 400px>
+        
+        if(newPhoto.type.match('image.*')){
+            let reader = new FileReader();
+    
+            reader.onload=function(newPhoto) {
+                console.log(newPhoto.target.result)
+                /*
+                postsView.innerHTML += `<div>
+                    <img src='"+${newPhoto}.target.result+"' width= 800px height= 400px>
+                    <p id="photoParagraph">${textBlog}</p>
+                </div>`
+                */
+            }
+        }
+        
+        
+        /*
+        postsView.innerHTML += `<div>
+        <img src="${newPhoto}" alt="examplePhoto" width= 800px height= 400px>
         <p id="photoParagraph">${textBlog}</p>
-        `
+        </div>`
+        */
     }
 }
 
